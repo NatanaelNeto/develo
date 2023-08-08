de204d99b430e80dfea515949d8eb5c85e5ffe06
Natanael Neto <natanael.eneas2018@hotmail.com>
2023-08-07 21:36:29 -0300

    C0 - Navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: NavbarComponent
+  },
+  {
+    path: 'time',
+    component: NavbarComponent
+  },
+  {
+    path: 'pomo',
+    component: NavbarComponent
+  },
+  {
+    path: 'memo',
+    component: NavbarComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
